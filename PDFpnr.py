125de953cf9ed97237b5dbf3511e6c0253df7efb
Andreas Segerberg <andreassegerberg@gmail.com>
2015-11-30 10:17:14 +0100

    Update PDFpnr.py

--- PDFpnr.py
+++ PDFpnr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import re
 import argparse
 import glob
@@ -9,6 +10,7 @@ import csv
 import time
 import datetime
 import fnmatch
+import codecs
 
 #Skapar argument som används i terminal/kommandoprompt 
 parser = argparse.ArgumentParser(description='Ett program som extraherar personnummer ur pdf-filer')
@@ -18,7 +20,7 @@ parser.add_argument('-n', nargs='?', const = 11, default = 11)
 args = parser.parse_args()
 
 #Funktion som används för att kontrollera giltiga tecken 
-def match(strg, search=re.compile(r'[^0-9-]').search):
+def match(strg, search=re.compile(r'[^0-9-TF ]').search):
     return not bool(search(strg))
     
 #Funktion som används för att skapa tidsstämplar 
@@ -28,8 +30,8 @@ def timestamp():
     return st
 
 #Öppna filer som information ska skrivas till 
-utfil = open(args.o, 'a')
-logfil = open('log.txt', 'a')
+utfil = codecs.open(args.o, 'a', encoding='utf-8')
+logfil = codecs.open('log.txt', 'a', encoding='utf-8')
 
 #Skapar headerinformation för CSV-filen 
 header = "filnamn,personnummer"+"\n" 
@@ -53,6 +55,9 @@ for file in matches:
     a = PdfFileReader(open(file, "rb"))
     b = a.getXmpMetadata()
     c = b.dc_subject
+    
+        
+
     file = file.split(args.p, 1)[1]
     
     #Kontrollera om metadata saknas 
@@ -62,23 +67,39 @@ for file in matches:
         logtext = stamp + text + file +'\n'
         errorcount = errorcount + 1
         logfil.write(logtext)
+        print(logtext)+'Metadata saknas '
         
     #Kontrollera att antalet tecken överensstämmer med det satta kriteriet i -n flaggan
     else:
         for item in c:
+
+            #Städar bort eventuell blanksteg 
+            item = item.replace(" ", "")
+            
             if len(item) != args.n:
                 stamp = timestamp()
                 text = '  2: '+ item 
                 logtext = stamp + text + file +'\n'
                 logfil.write(logtext)
+                print(logtext)+'Felaktig formatering av personnummer'
 
                 errorcount = errorcount + 1
+                itemcount = itemcount + 1
+                item2 = file +","+item+'\n'
+        
+                utfil.write(item2)
+                
             #Kontrollerar att personnummer innehåller giltiga tecken 
             elif match(item) is False:
                 stamp = timestamp()
                 text = '  3: '+ item 
                 logtext = stamp + text + file +'\n'
                 logfil.write(logtext)
+                itemcount = itemcount + 1
+                item2 = file +","+item+'\n'
+                utfil.write(item2)
+                errorcount = errorcount + 1
+                print(logtext)+'Personnummer innehaller ogiltiga tecken'
                 
             #Skriver till utfilen
             else:
